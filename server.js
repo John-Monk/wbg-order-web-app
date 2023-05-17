f3a95dba326b9502d05bd2df38342ee346dea51c
John Monk <johntmonk1@gmail.com>
2023-05-17 15:34:05 -0500

    added necessary dependencies, and server and database configurations. Made sure connection was working

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const PORT = 3000;
+require('dotenv').config();
+
+let db,
+    dbConnectionStr = process.env.DB_STRING;
+    dbName = 'restaurant'
+
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }).then(
+  (client) => {
+    console.log(`Connected to ${dbName} Database`);
+    db = client.db(dbName);
+  }
+);
+
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
