370058a9649dc04e320b9f2120844d3a0a35f011
John Monk <johntmonk1@gmail.com>
2023-05-18 14:08:19 -0500

    updated route for rendering menu data and array of pre-defined areas to verify on request. Added route for posting menu order to database

--- server.js
+++ server.js
@@ -20,9 +20,20 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/order', async (req, res) => {
+const diningAreas = ['table-1', 'table-2', 'table-3', 'table-4', 'table-5', 'table-7', 'table-8', 'table-9', 'table-10', 'bar', 'to-go'];
+
+app.get('/order/:id', async (req, res) => {
   const menuItems = await db.collection('menu').find().toArray();
-  
+  const areaId = req.params.id;
+  if(!diningAreas.includes(areaId)){
+    return res.status(403).send('Invalid dining area.')
+  }
+  res.render('order.ejs', { items: menuItems, area: areaId })
+})
+
+app.post('/order', async (req, res) => {
+  // db.collection('orders').insertOne({item: req.body.menuItem})
+  console.log(req.body)
 })
 
 app.listen(process.env.PORT || PORT, () => {
