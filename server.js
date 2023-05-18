26887b7221db68478cdf5d2e80dd9bbeae2d791d
John Monk <johntmonk1@gmail.com>
2023-05-18 12:02:04 -0500

    set view engine to ejs and added middleware for serving files from public dir, parsing url-encoded and json payloads

--- server.js
+++ server.js
@@ -15,6 +15,11 @@ MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }).then(
   }
 );
 
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.listen(process.env.PORT || PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
