2eb74f5ddd03b8bf78177a1c3eede7f06420c9e0
John Monk <johntmonk1@gmail.com>
2023-05-18 14:13:03 -0500

    changed area naming to location

--- server.js
+++ server.js
@@ -20,15 +20,15 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const diningAreas = ['table-1', 'table-2', 'table-3', 'table-4', 'table-5', 'table-7', 'table-8', 'table-9', 'table-10', 'bar', 'to-go'];
+const diningLocations = ['table-1', 'table-2', 'table-3', 'table-4', 'table-5', 'table-7', 'table-8', 'table-9', 'table-10', 'bar', 'to-go'];
 
 app.get('/order/:id', async (req, res) => {
   const menuItems = await db.collection('menu').find().toArray();
-  const areaId = req.params.id;
-  if(!diningAreas.includes(areaId)){
+  const locationId = req.params.id;
+  if(!diningLocations.includes(locationId)){
     return res.status(403).send('Invalid dining area.')
   }
-  res.render('order.ejs', { items: menuItems, area: areaId })
+  res.render('order.ejs', { items: menuItems, location: locationId })
 })
 
 app.post('/order', async (req, res) => {
